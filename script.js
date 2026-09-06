950bf3d0469ed42b6c9fc00e7c8d1b8d8b85065a
ExploiterX <ionutivan123gabriel@gmail.com>
2026-09-06 20:51:52 +0300

    Update GitHub username in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 ========================= */
 
 // SCHIMBĂ ASTA CU USERNAME-UL TĂU GITHUB
-const GITHUB_USERNAME = "ionutivan21";
+const GITHUB_USERNAME = "ExploiterX-16gb";
 
 
 /* =========================
